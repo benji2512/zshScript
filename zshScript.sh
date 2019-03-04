1f8ebd2de606b4423630c8b05621098110cb6821
Ben <benthedev@outlook.com>
2019-03-04 15:36:35 +0000

    Possible fix

--- zshScript.sh
+++ zshScript.sh
@@ -1,4 +1,3 @@
-#Only works on Ubuntu,Debian and derivitives. Also requires root priviliages
 cd ~
 NAME=$(lsb_release -is)
 if [ $NAME = 'Ubuntu' ]
@@ -8,14 +7,14 @@ elif [ $NAME = 'openSUSE' ]
 then
    sudo zypper install zsh;
 fi
-chsh -s ~/bin/zsh root
+sudo chsh -s /usr/bin/zsh root
 echo $SHELL
 if [ $NAME = 'Ubuntu' ]
 then
-   apt install wget git;
+   sudo apt install wget git;
 elif [ $NAME = 'openSUSE' ]
 then
-   zypper install wget git;
+   sudo zypper install wget git;
 fi
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 git clone https://github.com/benji2512/zshrcSetUp
