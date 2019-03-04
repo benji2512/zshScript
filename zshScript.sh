f54708bf560af5a94a91244b7a330b8b774f2990
Ben Clarke <benthedev@outlook.com>
2019-03-04 12:20:52 +0000

    Added is statements to handle install on OpenSuse

--- zshScript.sh
+++ zshScript.sh
@@ -1,9 +1,20 @@
 #Only works on Ubuntu,Debian and derivitives. Also requires root priviliages
 cd ~
-sudo apt install zsh
+osName = lsb_release -is
+if [osName == "Ubuntu"]
+then
+   sudo apt install zsh
+elif [osName == "openSUSE"]
+then
+   sudo zypper install zsh
 chsh -s ~/bin/zsh root
 echo $SHELL
-apt install wget git
+if [osName == "Ubuntu"]
+then
+   apt install wget git
+elif [osName == "openSUSE"]
+then
+   zypper install wget git
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 git clone https://github.com/benji2512/zshrcSetUp
 cp ~/zshrcSetUp/.zshrc ~/.zshrc
